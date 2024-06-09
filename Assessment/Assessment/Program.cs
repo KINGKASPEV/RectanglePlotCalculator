5d50dd2558eb62b4c553285858c97bcc8b0cd1ea
Kingkaspev <kingsleychiboy22@gmail.com>
2024-06-09 17:30:01 +0100

    Workdone

---
+++ Assessment/Assessment/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Assessment;
+
+Console.Write("Input the length of the plot: ");
+decimal length = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Input the breadth of the plot: ");
+decimal breadth = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Enter the cost per unit area: ");
+decimal costperunitare = Convert.ToDecimal(Console.ReadLine());
+
+Cost plot = new Cost(length, breadth, costperunitare);
+plot.CostOutput();
---
+++ Assessment/Assessment/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿namespace Assessment
+{
+    public class Rectangle
+    {
+        public decimal Length {  get; set; }
+        public decimal Breadth { get; set; }
+
+        public Rectangle(decimal length, decimal breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+
+        public decimal Area()
+        {
+            return Length * Breadth;
+        }
+
+       
+    }
+
+    public class Cost : Rectangle
+    {
+        private decimal CostPerArea {  get; set; }
+
+        public Cost(decimal length, decimal breadth, decimal costPerArea)
+            : base(length, breadth)
+        {
+            CostPerArea = costPerArea;
+        }
+
+        public decimal GetCost()
+        {
+            decimal area = Area();
+            return area * CostPerArea;
+        }
+
+        public void CostOutput()
+        {
+            decimal cost = GetCost();
+            Console.WriteLine($"Cost for rectangle plot is: {cost}");
+        }
+    }
+}
